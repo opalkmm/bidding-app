0a663ba3de5a94b20f05d8b2fe7699ca76c24480
Ornrumpa Kanchanomai <opalkm@Ornrumpas-MacBook-Pro.local>
2020-04-29 13:44:29 -0700

    replace new bid price to the selected item

--- logic.js
+++ logic.js
@@ -5,6 +5,9 @@ var inquirer = require("inquirer");
 //install mysql
 var mysql = require("mysql");
 
+//install table
+var Table = require('easy-table');
+
 //establish connection to the database
 var connection = mysql.createConnection({
   host: "localhost",
@@ -31,17 +34,18 @@ inquirer
     },
   ])
   .then(answers => {
-      var answers = JSON.stringify(answers.postOrBid);
-    console.log('answers :' + answers);
+      var answers1 = JSON.stringify(answers.postOrBid);
+    console.log('answers :' + answers1);
    
-    if (answers === answers) {
+    if (answers.postOrBid == "Bid on an item") {
       console.log("these are the available items for bidding...");
       showBids();
-    };
+    }
    
     //if answers === 'Post an item --> create input promt, add to biddingList
     //if answers === 'Bid an item --> show all items in the database in selection prompt
   })
+  
   .catch((error) => {
     if (error.isTtyError) {
       // Prompt couldn't be rendered in the current environment
@@ -56,9 +60,42 @@ inquirer
 function showBids() {
   connection.query("SELECT * FROM bids", function (err, res) {
     if (err) throw err;
-console.log(JSON.stringify(res))
-    // for (var i = 0; i < res.length; i++) {
-    //   console.log(res[i] + "\n");
+
+// var data = JSON.stringify(res);
+console.log(res);
+
+// create table for the bidding list
+var t = new Table
+  for (var i = 0; i < res.length; i++) {
     // }
-  });
-}
+    // data.forEach(function(bids){
+      t.cell('ID', res[i].id)
+      t.cell('Bidder', res[i].username)
+      t.cell('Items', res[i].items)
+      t.cell('Current bid price', res[i].bidPrice)
+      t.newRow()
+  };
+    console.log(Table.print(res))
+ //add a prompt to get the item the user wishes to bid on
+ inquirer
+ .prompt([
+   {
+     type: "input",
+     message: "enter the ID number of the item you wish to bid on",
+     name: "itemID",
+   },
+   {
+     message: "enter your bid price",
+     type: "input",
+     name: "newBidPrice"
+ }
+
+//if the new bid price is higher than the current, replace the row
+//if less or the same, leave it there 
+
+ ])
+
+ 
+ 
+  })
+}     
