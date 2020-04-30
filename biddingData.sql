d08ddf741754db69e25ef58848fd56304a293d4b
Ornrumpa Kanchanomai <opalkm@Ornrumpas-MacBook-Pro.local>
2020-04-29 18:13:51 -0700

    create option to post a new item or bid for the current ones - connected to DB

--- biddingData.sql
+++ biddingData.sql
@@ -8,4 +8,21 @@ username varchar(50) not null,
 items varchar(50) not null,
 bidPrice integer not null,
 primary key(id)
-)
+);
+drop table bids;
+
+select * from bids;
+
+insert into bids (username, items, bidPrice)
+values('alexH', 'airpods gen 1', 40),
+('Opalk', 'tamagotchi meets sanrio', 100),
+('RicardoH', 'Blythe Asha Alvira', 220),
+('MimiH', 'Star Twinkle Precure wand', 50);
+
+delete from bids where id = 2;
+
+insert into bids (id,username, items, bidPrice)
+values(2, 'alexH', 'moomin plush', 20);
+
+
+ 
--- logic.js
+++ logic.js
@@ -1,4 +1,3 @@
-
 //creating prompt in terminal
 var inquirer = require("inquirer");
 
@@ -6,7 +5,7 @@ var inquirer = require("inquirer");
 var mysql = require("mysql");
 
 //install table
-var Table = require('easy-table');
+var Table = require("easy-table");
 
 //establish connection to the database
 var connection = mysql.createConnection({
@@ -17,10 +16,6 @@ var connection = mysql.createConnection({
   database: "biddingData",
 });
 
-connection.connect(function (err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-});
 
 inquirer
   .prompt([
@@ -33,19 +28,24 @@ inquirer
       choices: ["Post an item", new inquirer.Separator(), "Bid on an item"],
     },
   ])
-  .then(answers => {
-      var answers1 = JSON.stringify(answers.postOrBid);
-    console.log('answers :' + answers1);
-   
+  .then((answers) => {
+    var choosePostOrBid = JSON.stringify(answers.postOrBid);
+    console.log("answers :" + choosePostOrBid + "\n ======================================================= \n");
+    //if answers === 'Bid an item --> show all items in the database in selection prompt
+
     if (answers.postOrBid == "Bid on an item") {
       console.log("these are the available items for bidding...");
       showBids();
     }
-   
-    //if answers === 'Post an item --> create input promt, add to biddingList
-    //if answers === 'Bid an item --> show all items in the database in selection prompt
-  })
-  
+        //if answers === 'Post an item --> create input promt, add to biddingList
+
+    if (answers.postOrBid == "Post an item"){
+      console.log("enter your username, item name, and your bid price");
+      addBids();
+    }
+
+    })
+
   .catch((error) => {
     if (error.isTtyError) {
       // Prompt couldn't be rendered in the current environment
@@ -61,41 +61,132 @@ function showBids() {
   connection.query("SELECT * FROM bids", function (err, res) {
     if (err) throw err;
 
-// var data = JSON.stringify(res);
-console.log(res);
+    // var data = JSON.stringify(res);
+    console.log(res);
 
-// create table for the bidding list
-var t = new Table
-  for (var i = 0; i < res.length; i++) {
-    // }
-    // data.forEach(function(bids){
-      t.cell('ID', res[i].id)
-      t.cell('Bidder', res[i].username)
-      t.cell('Items', res[i].items)
-      t.cell('Current bid price', res[i].bidPrice)
-      t.newRow()
-  };
-    console.log(Table.print(res))
- //add a prompt to get the item the user wishes to bid on
- inquirer
- .prompt([
-   {
-     type: "input",
-     message: "enter the ID number of the item you wish to bid on",
-     name: "itemID",
-   },
-   {
-     message: "enter your bid price",
-     type: "input",
-     name: "newBidPrice"
- }
+    // create table for the bidding list
+    var t = new Table();
+    for (var i = 0; i < res.length; i++) {
+      // }
+      // data.forEach(function(bids){
+      t.cell("ID", res[i].id);
+      t.cell("Bidder", res[i].username);
+      t.cell("Items", res[i].items);
+      t.cell("Current bid price", res[i].bidPrice);
+      t.newRow();
+    }
+    console.log(Table.print(res));
+    //add a prompt to get the item the user wishes to bid on
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "enter the ID number of the item you wish to bid on",
+          name: "itemID",
+        },
+        {
+          message: "enter your bid price",
+          type: "input",
+          name: "newBidPrice",
+        },
+      ])
+      .then((answers) => {
+        var idChosen = answers.itemID;
+        // console.log('item ID chosen :' + idChosen);
 
-//if the new bid price is higher than the current, replace the row
-//if less or the same, leave it there 
+        var newBidPrice = answers.newBidPrice;
+        console.log(
+          "your new bid on ID number " + idChosen + " is $" + newBidPrice
+        );
 
- ])
+        //quer the current bid price to compare with the user bid price input
+        connection.query(
+          "SELECT bidPrice FROM bids WHERE bids.id = " + idChosen + "",
+          function (err, res) {
+            if (err) throw err;
 
- 
- 
-  })
-}     
+            // console.log(res[0].bidPrice);
+
+            //store the current bid price of the selected item ID
+            var currentBidPrice = res[0].bidPrice;
+
+            //if the new bid price is higher than the current, replace the row
+            //if less or the same, "you lose the bid" and ask again which one you wish to bid?
+
+            if (newBidPrice > currentBidPrice) {
+              console.log(
+                "Congratulations! you are now the highest bidder of item id number " +
+                  idChosen
+              );
+              //replace new highest to the table in biddingData DB
+              connection.query(
+                "UPDATE bids SET bidPrice = " +
+                  newBidPrice +
+                  " WHERE id = " +
+                  idChosen +
+                  " ",
+                function (err, res) {
+                  if (err) throw err;
+
+                  console.log(
+                    "The current highest bid for this item is " + newBidPrice
+                  );
+                }
+              );
+            } else {
+              console.log(
+                "You lost the bid.. please reselect the item you wish to bid on"
+              );
+              showBids()
+            }
+          }
+        );
+      });
+  });
+}
+
+//function to insert new bid post
+function addBids(){
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "what is your username?",
+      name: "username",
+    },
+    {
+      type: "input",
+      message: "enter your item name",
+      name: "itemName",
+    },
+    {
+      type: "input",
+      message: "enter your bid price ($)",
+      name: "postBidPrice",
+      validate: validatePrice
+    }
+//validate input to number only
+
+
+
+  ])
+  .then((answers) => {
+    var username = answers.username;
+    var item = answers.itemName;
+    var postBidPrice = answers.postBidPrice;
+
+
+    console.log("username: " + username + " add an item " + item + " the a bid price of  " + postBidPrice);
+    var queryNow = 'INSERT INTO bids (username,items,bidPrice) VALUES ("'+username+'", "'+item+'", '+postBidPrice+')';
+    console.log(queryNow);
+    connection.query(queryNow)
+  }
+  )}
+//function to validate the bid price to be number only
+  function validatePrice(price)
+{
+   var reg = /^\d+$/;
+   return reg.test(price) || "Please enter a new price in numbers";
+}
+
+// connection.end()
